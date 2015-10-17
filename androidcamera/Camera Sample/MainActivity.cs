1cc680f1a9adaa230f75674cee49124591e3fee0
Vaikesh K P <vaikesh4leo@gmail.com>
2015-10-17 15:45:27 +0530

    Camera Sample in Xamarin.Android
    This sample will help to capture a picture using camera and view it in
    the image view.

---
+++ androidcamera/Camera Sample/MainActivity.cs
@@ -0,0 +1,123 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Provider;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace Camera_Sample
+{
+    [Activity(Label = "Camera Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        Java.IO.File _file;
+        Java.IO.File _dir;
+        ImageView _imgView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+
+                Button button = FindViewById<Button>(Resource.Id.MyButton);
+                _imgView = FindViewById<ImageView>(Resource.Id.imageView1);
+
+                button.Click += TakeAPicture;
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities = PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new Java.IO.File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures), "AndroidCameraVSDemo");
+            if (!_dir.Exists())
+            {
+                _dir.Mkdirs();
+            }
+        }
+
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+            _file = new Java.IO.File(_dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(_file));
+
+            StartActivityForResult(intent, 0);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (resultCode == Result.Canceled) return;
+            // make it available in the gallery
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            if (_file != null)
+            {
+                Android.Net.Uri contentUri = Android.Net.Uri.FromFile(_file);
+                mediaScanIntent.SetData(contentUri);
+                SendBroadcast(mediaScanIntent);
+
+
+                // display in ImageView. We will resize the bitmap to fit the display
+                // Loading the full sized image will consume to much memory 
+                // and cause the application to crash.
+                int height = _imgView.Height;
+                int width = Resources.DisplayMetrics.WidthPixels;
+                using (Bitmap bitmap = _file.Path.LoadAndResizeBitmap(width, height))
+                {
+                    _imgView.SetImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+
+    public static class BitmapHelpers
+    {
+        public static Bitmap LoadAndResizeBitmap(this string fileName, int width, int height)
+        {
+            // First we get the the dimensions of the file on disk
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+            BitmapFactory.DecodeFile(fileName, options);
+
+            // Next we calculate the ratio that we need to resize the image by
+            // in order to fit the requested dimensions.
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight
+                                   ? outHeight / height
+                                   : outWidth / width;
+            }
+
+            // Now we will load the image and have BitmapFactory resize it for us.
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+
+            return resizedBitmap;
+        }
+    }
+}
+
